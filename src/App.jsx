6896e98a038f3749a8669085edaff09dd078d57f
JCordova-Growbiz <jcordova@growbiz.la>
2025-07-10 18:16:12 -0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,65 @@ import { CustomEase } from "gsap/CustomEase";
 gsap.registerPlugin(Flip, CustomEase);
 
 export default function App() {
-  /*  useGSAP(() => {
-    const tl = gsap.timeline();
-
-    const cardContainer = document.querySelector("#card1");
-    const background = document.querySelector(".background");
-
-    const state = Flip.getState(background);
-
-    cardContainer.appendChild(background);
-
-    const flipAnimation = Flip.from(state, {
-      duration: 2,
-      ease: "expo.inOut",
-      scale: true,
-    });
-  }); */
-
-  useGSAP(()=>{
+  useGSAP(() => {
     const tl = gsap.timeline();
 
     tl.from(".overlay-span", {
+      delay: 0.5,
       yPercent: 130,
-      stagger: .3,
+      stagger: 0.3,
       ease: "power1.inOut",
-    })
-    .from(".overlay-img", {
-      scale: 1.2,
       autoAlpha: 0,
-      ease: "power1.inOut"
     })
-  })
+      .from(".overlay-img", {
+        duration: 1,
+        scale: 1.3,
+        autoAlpha: 0,
+        ease: "power3.out",
+      })
+      .from(".logo-top", {
+        yPercent: -150,
+        ease: "power1.out",
+      })
+      .from(
+        ".logo-bottom",
+        {
+          yPercent: 150,
+          ease: "power1.out",
+          stagger: 0.1,
+        },
+        "<"
+      )
+      .to(
+        ".overlay-span",
+        {
+          y: "-100%",
+          autoAlpha: 0,
+          stagger: 0.3,
+          ease: "power1.inOut",
+        },
+        "-=.8"
+      )
+      .to(".logo-top", {
+        y: "608%",
+      })
+      .to(
+        ".letra-1",
+        {
+          x: -337,
+          color: "#1e1e1e",
+        },
+        "<"
+      )
+      .to(
+        ".letra-2",
+        {
+          x: 337,
+          color: "#1e1e1e",
+        },
+        "<"
+      );
+  });
 
   return (
     <>
@@ -99,59 +127,59 @@ export default function App() {
 
           {/* FOOTER TITLES */}
           <div className="max-h-full h-full flex w-full text-[#1e1e1e]">
-            <span className="flex self-end-safe text-span font-FKScreamer">
+            <span className="padre flex self-end-safe text-span font-FKScreamer">
               JECY
             </span>
+
             <div className="flex flex-col py-6 w-full h-full items-center justify-end gap-21 text-center">
-              <div className="font-NeueMontreal-Bold text-[28.5px] leading-[100%]">
+              <span className="font-NeueMontreal-Bold text-[28.5px] leading-[100%]">
                 ©JAMAREA <br />
                 AGENZIA DI COMUNICAZIONE <br />
                 BRANDING <br />E DIGITAL MARKETING
-              </div>
+              </span>
 
               <div className="font-NeueMontreal-Bold text-xs">
                 <span>LET'S JAM</span>
               </div>
             </div>
-            <span className="flex self-end-safe text-span font-FKScreamer">
+
+            <span className="padre2 flex self-end-safe text-span font-FKScreamer">
               NSIDE
             </span>
           </div>
         </div>
       </main>
 
+      <div className="ovarlay flex flex-col fixed inset-0 justify-center items-center bg-transparent">
+          <div className="relative z-1 flex gap-5 font-NeueMontreal-Bold text-white">
+            <span className="overlay-span">LET'S JAM TOGETHER</span>
+            <span className="overlay-span">RIGHT NOW</span>
+            <span className="overlay-span">OH YEA</span>
+          </div>
 
+          <div className="container-image absolute size-full">
+            <img
+              className="overlay-img"
+              src={gridImages.Img1}
+              alt="img-overlay"
+            />
+          </div>
 
-      <div className="flex flex-col fixed inset-0 justify-center items-center bg-black">
+          <span className="logo-top absolute z-50 text-red-600 font-NeueMontreal-Bold text-[28.5px] leading-[100%] top-7 text-center">
+            ©JAMAREA <br />
+            AGENZIA DI COMUNICAZIONE <br />
+            BRANDING <br />E DIGITAL MARKETING
+          </span>
 
-
-        <div className="relative z-1 flex gap-5 font-NeueMontreal-Bold overflow-hidden text-white">
-          <span className="relative overlay-span">LET'S JAM TOGETHER</span>
-          <span className="relative overlay-span">RIGHT NOW</span>
-          <span className="relative overlay-span">OH YEA</span>
-        </div>
-
-
-        <div className="absolute size-full">
-            <img className="overlay-img" src={gridImages.Img4} alt="img-overlay" />
-        </div>
-
+          <div className="absolute flex text-white bottom-0 gap-10">
+            <span className="letra-1 flex self-end-safe text-span font-FKScreamer">
+              JECY
+            </span>
+            <span className="letra-2 flex self-end-safe text-span font-FKScreamer">
+              NSIDE
+            </span>
+          </div>
       </div>
-
-
-
-
-
-
-
-
-
-
-      {/* <img
-        className="background absolute z-50 size-full top-0"
-        src={gridImages.Img1}
-        alt="Background"
-      /> */}
     </>
   );
 }
